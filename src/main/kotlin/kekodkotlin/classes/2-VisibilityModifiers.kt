305771293164ba170d8c239971626011f2a7fa39
Mehmet Ali Öyür <mehmetalioyur@gmail.com>
2024-10-29 18:19:19 +0300

    add class visibility modifiers lesson

---
+++ src/main/kotlin/kekodkotlin/classes/2-VisibilityModifiers.kt
@@ -0,0 +1,20 @@
+package kekodkotlin.classes
+
+
+/**
+ * Kotlin'de kullanabileceğimiz 4 farklı visibility modifier vardır.
+ * Bunlar: Public, protected, internal ve private'dır.
+ *
+ * Public herkese açık.
+ * Private herkese kapalı.
+ *
+ *
+ * Kotlin'de classlar default public olarak bulunur. Java'da bu private'dır.
+ * Bunun sebebi kotlin'in değişkenin kendisine erişmemize izin vermemesidir.
+ * class içindeki backing field'ı olan propertyler public olsa dahi backing field'a setter ve getter dışından erişemeyiz.
+ * private ve public keywordleri getter ve setterı oluşturur ya da oluşturmaz.
+ *
+ *
+ *
+ */
+
